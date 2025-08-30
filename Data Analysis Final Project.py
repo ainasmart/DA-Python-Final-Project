28873efb5e6a8d6e1baeb885b0ca8babf67deff4
ainasmart <trainingcottage@gmail.com>
2025-08-30 08:37:49 +0100

    Update Data Analysis Final Project.py

--- Data Analysis Final Project.py
+++ Data Analysis Final Project.py
@@ -32,40 +32,40 @@ df["CATEGORY"].value_counts()
 df["WINNER"].value_counts()
 
 #Filter out rows where WINNER is 0 (numeric) or '0' (string)
-df_win = df[df["WINNER"] != 0]
+df = df[df["WINNER"] != 0]
 
 #Shape of the new cleaned data
-df_win.shape
+df.shape
 
 #Drop the "WINNER" column from the cleaned data to ensure the dataset 
-df_win.drop(['WINNER'], axis=1, inplace=True) 
+df.drop(['WINNER'], axis=1, inplace=True) 
 
 #Shape of the new cleaned data
-df_win.shape
+df.shape
 
 # Getting the null value counts along each rows.
-df_win.isnull().sum().sort_values()
+df.isnull().sum().sort_values()
 
-df_win["AGE"].value_counts().sort_values()
+df["AGE"].value_counts().sort_values()
 
-df_win["EDUCATION"].value_counts()
+df["EDUCATION"].value_counts()
 
-df_win["GENDER"].value_counts()
-df_win["PARTY"].value_counts()
-df_win.info()
+df["GENDER"].value_counts()
+df["PARTY"].value_counts()
+df.info()
 
 
 #EDA
 
 # 1. GENDER-AGE SPREAD AMONG WINNING ELECTORS.
-gender_age = df_win.groupby("GENDER")["AGE"].mean().sort_values().head(10)
+gender_age = df.groupby("GENDER")["AGE"].mean().sort_values().head(10)
 plt.barh(y= gender_age.index, width = gender_age.values)
 plt.xlabel("Age")
 plt.ylabel("Gender")
 plt.title("Male vs Female (Winning) Electors");
 
 # 2. TOTAL WINNING VOTES ACROSS THE STATES
-party_state = df_win.groupby("STATE")["TOTAL\nVOTES"].mean().sort_values().head(20)
+party_state = df.groupby("STATE")["TOTAL\nVOTES"].mean().sort_values().head(20)
 plt.barh(y= party_state.index, width = party_state.values)
 plt.xlabel("Winning Votes")
 plt.ylabel("State")
